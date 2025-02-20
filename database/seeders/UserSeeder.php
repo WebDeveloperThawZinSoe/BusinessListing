8032aeb640c8c38311d2794b0f2bb45b0bec0bf7
Thaw Zin Soe <63851168+WebDeveloperThawZinSoe@users.noreply.github.com>
2025-02-20 11:36:19 +0630

    Database Seeding

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'),
+        ]);
+        $admin->assignRole('admin');
+
+        $shop = User::create([
+            'name' => 'Shop Owner',
+            'email' => 'shop@example.com',
+            'password' => bcrypt('password'),
+        ]);
+        $shop->assignRole('shop');
+
+        $user = User::create([
+            'name' => 'Regular User',
+            'email' => 'user@example.com',
+            'password' => bcrypt('password'),
+        ]);
+        $user->assignRole('user');
+    }
+}
